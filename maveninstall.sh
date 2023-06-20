72934d9557dab5e78be686165b66988abb0150a9
loopNet1 <116150945+loopNet1@users.noreply.github.com>
2023-06-20 12:16:42 +0530

    Update maveninstall.sh

--- maveninstall.sh
+++ maveninstall.sh
@@ -1,7 +1,7 @@
 pwd
 # apt-get install default-jdk -y
-sudo apt-get install openjdk-11-jdk
-sudo apt-get install maven -y
-sudo mvn -v
-sudo mvn clean package
+# sudo apt-get install openjdk-11-jdk
+# sudo apt-get install maven -y
+mvn -v
+#sudo mvn clean package
 ls
