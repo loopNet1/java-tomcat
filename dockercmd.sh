af52dda2c2b16144585b9fd636d92e267ff768a7
loopNet1 <116150945+loopNet1@users.noreply.github.com>
2023-06-20 14:23:14 +0530

    Update dockercmd.sh

--- dockercmd.sh
+++ dockercmd.sh
@@ -1,5 +1,5 @@
-docker build -t omkarphadtare321/java-tom .  # Build docker image frm dockerfile
-docker run -itd -p 8080:8080  --name java-project omkarphadtare321/java-tom  #create container using docker image 
+sudo docker build -t omkarphadtare321/java-tom .  # Build docker image frm dockerfile
+sudo docker run -itd -p 8080:8080  --name java-project omkarphadtare321/java-tom  #create container using docker image 
 sudo docker ps 
 #docker inspect fineshift_emp_info  # getting info about container     
 sudo docker ps
